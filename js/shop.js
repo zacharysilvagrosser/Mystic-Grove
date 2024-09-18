b2e62f2ecf213b865e13c84064cc7837fc046b6c
zacharysilvagrosser <35900677+zacharysilvagrosser@users.noreply.github.com>
2024-09-18 13:49:15 -0700

    Added Cart Icon and CSS Update
    Updated CSS to have all first h1's margin change from a media query so theres not a large gap where the nav bar used to be

--- js/shop.js
+++ js/shop.js
@@ -1,29 +1,29 @@
-$('.slick-forest').slick({
+$('.slick-shop').slick({
     slidesToShow: 5,
     slidesToScroll: 5,
     prevArrow: '<i class="fa-solid fa-arrow-left fa-2x"></i>',
     nextArrow: '<i class="fa-solid fa-arrow-right fa-2x"></i>',
     responsive: [
-        {
-          breakpoint: 1200,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 3,
-          },
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
         },
-        {
-          breakpoint: 1008,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 3,
-          },
+      },
+      {
+        breakpoint: 1000,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
         },
-        {
-          breakpoint: 800,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2,
-          },
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
         },
-      ],
+      },
+    ],
 });
